73816535e38b3bc193fd16535f4bedc1df3ef7b2
DataFire <info@datafire.io>
2019-06-17 14:33:25 -0400

    Update master for deployment

--- actions/contact_form.js
+++ actions/contact_form.js
@@ -28,7 +28,9 @@ module.exports = new datafire.Action({
     }, context);
     let message = await google_gmail.users.messages.send({
       userId: "me",
-      body: {raw: encodedMessage},
+      body: {
+        raw: encodedMessage,
+      },
     }, context);
     return message;
   },

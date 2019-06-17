7be1fe064ce4bd1a6b7a8346f827e70c26bde603
DataFire <info@datafire.io>
2019-06-17 10:33:00 -0400

    initial commit

--- actions/contact_form.js
+++ actions/contact_form.js
@@ -4,7 +4,9 @@ let datafire = require('datafire');
 let google_gmail = require('@datafire/google_gmail').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let encodedMessage = await google_gmail.buildMessage({}, context);
+    let encodedMessage = await google_gmail.buildMessage({
+      to: "oliviakgosselin@gmail.com:",
+    }, context);
     let message = await google_gmail.users.messages.send({
       userId: oliviakgosselin,
     }, context);
